497165c69d1ff52367c2c9ee6c54b7c92e0eca11
abdulrehman192 <mr.abdulrehman.ar@gmail.com>
2024-03-19 14:54:39 +0500

    delete unwanted files

--- wp-content/plugins/bookingpress-waiting-list/core/views/upgrade_latest_waiting_list_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $BookingPress, $bookingpress_waiting_list_version, $wpdb;
-$bookingpress_old_waiting_list_version = get_option('bookingpress_waiting_list_version', true);
-
-
-$bookingpress_waiting_list_new_version = '1.5';
-update_option('bookingpress_waiting_list_version', $bookingpress_waiting_list_new_version);
-update_option('bookingpress_waiting_list_version_date_' . $bookingpress_waiting_list_new_version, current_time('mysql'));
--- wp-content/plugins/templately/assets/js/gutenberg.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-data', 'wp-editor', 'wp-hooks'), 'version' => 'bc74b87c5e4e0756db66');
